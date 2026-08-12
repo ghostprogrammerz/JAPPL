a213a8a3a51b5600973c68ec6fc8ca73da2de503
SCdogcat <seancathancabral@gmail.com>
2026-08-12 21:49:11 +0530

    Wire decompiler syntax adapter into organized decompiler module

--- app/main.c
+++
@@ -1,4 +0,0 @@
-#include "decompiler/decompiler_fix.c"
-#define decompile_sb3 decompile_sb3_compiler_syntax
-#include "app/main.c"
-#undef decompile_sb3
--- decompiler.c
+++ decompiler.c
@@ -1,1 +1,11 @@
+#define decompile_sb3 decompile_sb3_raw
 #include "decompiler/decompiler.c"
+#undef decompile_sb3
+
+#define decompile_sb3 decompile_sb3_raw
+#include "decompiler/decompiler_fix.c"
+#undef decompile_sb3
+
+int decompile_sb3(const char *sb3_path, const char *out_zip_path) {
+    return decompile_sb3_compiler_syntax(sb3_path, out_zip_path);
+}
